a6f52131855839dae6a5d023272a7fd8feacb876
Sergey_Opihaylenko <sergomail@bk.ru>
2022-09-10 22:51:02 +0300

    edit

--- src/app/components/examples/useRef/programmableActions.jsx
+++ src/app/components/examples/useRef/programmableActions.jsx
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useRef } from "react";
+import Divider from "../../common/divider";
 import CardWrapper from "../../common/Card";
 import SmallTitle from "../../common/typografy/smallTitle";
 const ProgrammableActionsExample = () => {
+    const inputRef = useRef();
+    const handleClick = () => {
+        console.log(inputRef.current);
+    };
     return (
         <CardWrapper>
             <SmallTitle className="card-title">
                 Программируемые действия и свойства
             </SmallTitle>
+            <Divider />
+            <label htmlFor="" className="form-label"></label>
+            <input
+                ref={inputRef}
+                type="email"
+                className="form-control"
+                id="email"
+            />
+            <button className="btn btn-primary" onClick={handleClick}>
+                Кнопка
+            </button>
         </CardWrapper>
     );
 };
