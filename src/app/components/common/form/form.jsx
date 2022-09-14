78a1a425394820ef286f25720a0e50a7a477f444
Sergey_Opihaylenko <sergomail@bk.ru>
2022-09-15 00:09:30 +0300

    refactoring

--- src/app/components/common/form/form.jsx
+++ src/app/components/common/form/form.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { validator } from "../../../utils/validator";
 import PropTypes from "prop-types";
 
@@ -10,35 +10,52 @@ const FormComponent = ({
 }) => {
     const [data, setData] = useState(defaultData || {});
     const [errors, setErrors] = useState({});
-    const handleChange = (target) => {
-        setData((prevState) => ({
-            ...prevState,
-            [target.name]: target.value
-        }));
-    };
+    const handleChange = useCallback(
+        (target) => {
+            setData((prevState) => ({
+                ...prevState,
+                [target.name]: target.value
+            }));
+        },
+        [setData]
+    );
+
+    const validate = useCallback(
+        (data) => {
+            const errors = validator(data, validatorConfig);
+            setErrors(errors);
+            return Object.keys(errors).length === 0;
+        },
+        [validatorConfig, setErrors]
+    );
+
     const handleSubmit = (e) => {
         e.preventDefault();
         const isValid = validate();
         if (!isValid) return;
         onSubmit(data);
-
-        console.log(data);
     };
     useEffect(() => {
         if (Object.keys(data).length > 0) {
-            validate();
+            validate(data);
         }
     }, [data]);
+
+    const handleKeyDown = useCallback((event) => {
+        if (event.keyCode === 13) {
+            event.preventDefault();
+            const form = event.target.form;
+            const indexField = Array.prototype.indexOf.call(form, event.target);
+            form.elements[indexField + 1].focus();
+        }
+    }, []);
+
     const isValid = Object.keys(errors).length === 0;
-    const validate = () => {
-        const errors = validator(data, validatorConfig);
-        setErrors(errors);
-        return Object.keys(errors).length === 0;
-    };
+
     const clonedElements = React.Children.map(children, (child) => {
         const childType = typeof child.type;
         let config;
-        if (childType === "function") {
+        if (childType === "object") {
             if (!child.props.name) {
                 throw new Error(
                     "Name property is required for field components",
@@ -49,7 +66,8 @@ const FormComponent = ({
                 ...child.props,
                 onChange: handleChange,
                 value: data[child.props.name] || "",
-                error: errors[child.props.name]
+                error: errors[child.props.name],
+                onKeyDown: handleKeyDown
             };
         }
         if (childType === "string") {
